39dda4aa10a7e03e94bce3bb9017426d574da3b3
WinnieChun1234 <63451005+WinnieChun1234@users.noreply.github.com>
2020-05-09 19:14:32 +0800

    Align naming style
    Align naming style

--- action.cpp
+++ action.cpp
@@ -6,77 +6,77 @@
 
 using namespace std;
 
-stack<Action*> undoStack;
-stack<Action*> redoStack;
+stack<Action*> undo_stack;
+stack<Action*> redo_stack;
 
 //to empty both undo and redo stack
-void resetStacks() {
+void reset_stacks() {
     //keep popping and deleting action in the redo stack
-    while(!redoStack.empty()) {
-        delete redoStack.top();
-        redoStack.pop();
+    while(!redo_stack.empty()) {
+        delete redo_stack.top();
+        redo_stack.pop();
     }
-
+    
     //keep popping and deleting action in the undo stack
-    while(!undoStack.empty()) {
-        delete undoStack.top();
-        undoStack.pop();
+    while(!undo_stack.empty()) {
+        delete undo_stack.top();
+        undo_stack.pop();
     }
 }
 
 //to add action to undo stack (this function to be called when user performed an action)
 //input:
 //Action* a: the action to be added
-void addAction(Action* a) {
+void add_action(Action* a) {
     //empty the redo stack
-    while(!redoStack.empty()) {
-        delete redoStack.top();
-        redoStack.pop();
+    while(!redo_stack.empty()) {
+        delete redo_stack.top();
+        redo_stack.pop();
     }
-
+    
     //push the action to the undo stack
-    undoStack.push(a);
+    undo_stack.push(a);
 }
 
 //pop an action from the undo stack and undo
 void undo(vector<vector<Cell>>& board) {
-    if(undoStack.empty()) {
+    if(undo_stack.empty()) {
         return;
     }
-
-    Action* a = undoStack.top();
-    undoStack.pop();
-    redoStack.push(a);  //the poped action will be pushed to the redo stack
+    
+    Action* a = undo_stack.top();
+    undo_stack.pop();
+    redo_stack.push(a);  //the poped action will be pushed to the redo stack
     board[a->y][a->x].status = a->from; //undo the changes on the board according to the recorded action
 }
 
 //pop an action from the redo stack and redo
 void redo(vector<vector<Cell>>& board) {
-    if(redoStack.empty()) {
+    if(redo_stack.empty()) {
         return;
     }
-
-    Action* a = redoStack.top();
-    redoStack.pop();
-    undoStack.push(a);  //the poped action will be pushed to the undo stack
+    
+    Action* a = redo_stack.top();
+    redo_stack.pop();
+    undo_stack.push(a);  //the poped action will be pushed to the undo stack
     board[a->y][a->x].status = a->to;   //redo the action on the board according to the recorded action
 }
 
-//get a pointer to the undoStack as output
-stack<Action*>* getUndoStack() {
-    return &undoStack;
+//get a pointer to the undo_stack as output
+stack<Action*>* get_undo_stack() {
+    return &undo_stack;
 }
 
-//get a pointer to the redoStack as output
-stack<Action*>* getRedoStack() {
-    return &redoStack;
+//get a pointer to the redo_stack as output
+stack<Action*>* get_redo_stack() {
+    return &redo_stack;
 }
 
 //to convert the action into plain text
 //input:
 //Action* a: the action to be converted
 //output: the created string
-string actionToStr(Action* a) {
+string action_to_string(Action* a) {
     stringstream buffer;
     buffer << a->y << " " << a->x << " " << a->from << " " << a->to << endl;
     //e.g.: "2 3 0 1"
@@ -87,7 +87,7 @@ string actionToStr(Action* a) {
 //input:
 //string s: the string to be converted
 //output: the created action
-Action* stringToAction(string s) {
+Action* string_to_action(string s) {
     stringstream buffer(s);
     Action* a = new Action();
     buffer >> a->y >> a->x;
@@ -100,28 +100,28 @@ Action* stringToAction(string s) {
 }
 
 //print out the undo and redo stack for debug use
-void printStack() {
+void print_stacks() {
     //duplicate stacks
-    stack<Action*> tmp1(undoStack);
-    stack<Action*> tmp2(redoStack);
+    stack<Action*> tmp1(undo_stack);
+    stack<Action*> tmp2(redo_stack);
     cout << "Undo Stack from top to bottom:" << endl;
-
+    
     while(tmp1.size() > 0) {
         Action* a = tmp1.top();
         cout << "Y: " << a->y << " X: " << a->x << " From: " << a->from << " To: " << a->to << "       " << endl;
         //e.g.: "Y: 2 X: 3 From: 0 To: 1       "
         tmp1.pop();
     }
-
+    
     cout << endl << endl;
     cout << "Redo Stack from top to bottom:" << endl;
-
+    
     while(tmp2.size() > 0) {
         Action* a = tmp2.top();
         cout << "Y: " << a->y << " X: " << a->x << " From: " << a->from << " To: " << a->to << "       " << endl;
         //e.g.: "Y: 2 X: 3 From: 0 To: 1       "
         tmp2.pop();
     }
-
+    
     cout << "======END=====" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -34,11 +34,11 @@ int readkey() {
 
 enum GAME_MODE { PRACTICE = 1, NORMAL = 2 };
 void start_tutorial();
-void start_game(GAME_MODE, vector<vector<Cell>>* savedBoard = NULL);
+void start_game(GAME_MODE, vector<vector<Cell>>* saved_board = NULL);
 void settings_screen();
 void load_screen();
 void exit_screen();
-void exportGame(vector<vector<Cell>> board, GAME_MODE mode);
+void export_game(vector<vector<Cell>> board, GAME_MODE mode);
 void print_board(vector<vector<Cell>>);
 
 void cls() { //clear screen
@@ -55,16 +55,16 @@ int main(int argc, char* argv[]) {
     if(argc > 1 && !strcmp(argv[1], "-d")) {
         debug_mode = true;
     }
-
+    
     string c;
-
+    
     while(1) {
         cls();
-
+        
         if(debug_mode) {
             cout << "debug mode enabled" << endl;
         }
-
+        
         cout << endl;
         cout << "   ██████  ▒█████   ██▓     ██▓▄▄▄█████▓ █    ██ ▓█████▄  ▒█████   " << endl;
         cout << "▒██    ▒ ▒██▒  ██▒▓██▒    ▓██▒▓  ██▒ ▓▒ ██  ▓██▒▒██▀ ██▌▒██▒  ██▒  " << endl;
@@ -84,12 +84,12 @@ int main(int argc, char* argv[]) {
         cout << "5. Load Saved Game" << endl;
         cout << "6. Exit" << endl;
         cout << endl;
-
+        
         while(1) { //keep looping until getting the correct input, and go to the destination screen
             cout << "Please enter the number and press enter: ";
             cin >> c;
             cin.ignore(); //ignore the enter key, to prevent the enter key mistakenly read by the readkey() function
-
+            
             if(c == "1") {
                 start_tutorial();
             } else if(c == "2") {
@@ -107,7 +107,7 @@ int main(int argc, char* argv[]) {
                 cout << "INVALID INPUT" << endl;
                 continue;
             }
-
+            
             break;
         }
     }
@@ -124,33 +124,34 @@ void exit_screen() {
 void start_tutorial() {
     cls();
     //create preset board
-    int presetBoard[6][6] = {5, 6, 5, 1, 2, 2,
-                             3, 2, 4, 5, 1, 3,
-                             2, 3, 2, 6, 6, 4,
-                             3, 4, 5, 6, 3, 6,
-                             4, 5, 4, 3, 2, 1,
-                             3, 4, 2, 4, 4, 5};
+    int preset_board[6][6] = {5, 6, 5, 1, 2, 2,
+                              3, 2, 4, 5, 1, 3,
+                              2, 3, 2, 6, 6, 4,
+                              3, 4, 5, 6, 3, 6,
+                              4, 5, 4, 3, 2, 1,
+                              3, 4, 2, 4, 4, 5
+                             };
     vector<vector<Cell>> board(6);
-
+    
     for(int i = 0; i < 6; i++) {
         vector<Cell> m(6);
-
+        
         for(int j = 0; j < 6; j++) {
-            m[j].n = presetBoard[j][i];
+            m[j].n = preset_board[j][i];
             m[j].status = UNMARKED;
         }
-
+        
         board[i] = m;
     }
-
-    int pageNumber = 0;
-    bool isFinished = false;
-
-    while(!isFinished) {
+    
+    int page_number = 0;
+    bool is_finished = false;
+    
+    while(!is_finished) {
         cls();
         string temp = "";
-
-        switch(pageNumber) {
+        
+        switch(page_number) {
         case 0:
             temp += "The goal of the game is to blacken some cells\n";
             temp += "such that the following rules are not violated:\n";
@@ -164,104 +165,104 @@ void start_tutorial() {
             temp += "This tutorial will provide some basic solving techniques,\n";
             temp += "derived from the above rules. Let's get started! ^v^\n";
             break;
-
+            
         case 1:
             temp += "- Search for a cell in between cells with same numbers\n";
             temp += "As the two outer cells cannot be both white (Rule 2),\n";
             temp += "at least one of them should be black. Therefore,\n";
             temp += "the middle cell should be marked white (Rule 1).\n";
-
+            
             for(int n : {22}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             cur_i = 22 % 6;
             cur_j = 22 / 6;
             break;
-
+            
         case 2:
             temp += "- Search for a cell in between cells with same numbers\n";
             temp += "These are all the cells that matches this pattern.\n";
-
+            
             for(int n : {1, 12, 13, 22, 25, 32}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             break;
-
+            
         case 3:
             temp += "- Blacken cells in the same row/column as a white cell with the same number\n";
             temp += "This technique is derived from Rule 2.\n";
-
+            
             for(int n : {14}) {
                 board[n % 6][n / 6].status = BLACK;
             }
             cur_i = 14 % 6;
             cur_j = 14 / 6;
             break;
-
+            
         case 4:
             temp += "- Blacken cells in the same row/column as a white cell with the same number\n";
             temp += "These are all the cells that matches this pattern.\n";
-
+            
             for(int n : {14, 18}) {
                 board[n % 6][n / 6].status = BLACK;
             }
             break;
-
+            
         case 5:
             temp += "- Whiten cells adjacent to a black cell\n";
             temp += "This technique is derived from Rule 1.\n";
-
+            
             for(int n : {15}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             cur_i = 15 % 6;
             cur_j = 15 / 6;
             break;
-
+            
         case 6:
             temp += "- Whiten cells adjacent to a black cell\n";
             temp += "These are all the cells that matches this pattern.\n";
-
+            
             for(int n : {8, 15, 19, 20, 24}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             break;
-
+            
         case 7:
             temp += "- Blacken cells in the same row/column as a white cell with the same number\n";
             temp += "- Whiten cells adjacent to a black cell\n";
             temp += "These two techniques will be the most likely to be used.\n";
             temp += "Check frequently to see if these can be applied.\n";
-
+            
             for(int n : {0, 3, 7, 10, 17, 27, 30}) {
                 board[n % 6][n / 6].status = WHITE;
             }
-
+            
             for(int n : {2, 6, 16, 21, 26, 31}) {
                 board[n % 6][n / 6].status = BLACK;
             }
             break;
-
+            
         case 8:
             temp += "- Whiten cell that will separate other white cells in two otherwise\n";
             temp += "This technique is derived from Rule 3.\n";
-
+            
             for(int n : {33}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             cur_i = 33 % 6;
             cur_j = 33 / 6;
             break;
-
+            
         case 9:
             temp += "- Whiten cell that will separate other white cells in two otherwise\n";
             temp += "These are all the cells that matches this pattern.\n";
-
+            
             for(int n : {9, 11, 23, 33}) {
                 board[n % 6][n / 6].status = WHITE;
             }
             break;
-
+            
         case 10:
             temp += "There are other techniques to be discovered.\n";
             temp += "Try to come up one on your own! ;)\n";
@@ -269,32 +270,32 @@ void start_tutorial() {
             temp += "one solved states, but any one of them will be reconized as a solution.\n";
             temp += "\n";
             temp += "Have fun playing Solitudo! ^o^\n";
-
+            
             for(int n : {5, 28, 29, 35}) {
                 board[n % 6][n / 6].status = WHITE;
             }
-
+            
             for(int n : {4, 34}) {
                 board[n % 6][n / 6].status = BLACK;
             }
-
+            
         default:
-            isFinished = true;
+            is_finished = true;
         }
-
+        
         cout << "TUTORIAL MODE" << endl << endl;
-
-        if(pageNumber != 0) {
+        
+        if(page_number != 0) {
             print_board(board);
         }
-
+        
         cout << endl;
         cout << temp << endl;
-        pageNumber++;
+        page_number++;
         cout << "Press any key to continue." << endl;
         readkey();
     }
-
+    
     return;
 }
 
@@ -302,18 +303,18 @@ void start_tutorial() {
 //print the given board onto the screen
 void print_board(vector<vector<Cell>> board) {
     printf("     ");
-
+    
     //display column label
     for(int i = 0; i < board[0].size(); i++) {
         printf(" %c ", 'A' + i);
     }
-
+    
     printf("\n");
     printf("\n");
-
+    
     for(int i = 0; i < board.size(); i++) {//for each row
         printf("%2d   ", i + 1);    //display row label
-
+        
         for(int j = 0; j < board[i].size(); j++) {//for each cell
             if(i == cur_i && j == cur_j) { //if the cursor is in this cell, show the number with square bracket, e.g. [1]
                 if(board[i][j].status == WHITE) {
@@ -333,74 +334,74 @@ void print_board(vector<vector<Cell>> board) {
                 }
             }
         }
-
+        
         printf("\n");
     }
 }
 
 //start the game in with the game in specified mode with a saved board (if any)
-void start_game(GAME_MODE mode, vector<vector<Cell>>* savedBoard) {
+void start_game(GAME_MODE mode, vector<vector<Cell>>* saved_board) {
     cls();
     string game_mode_str;
-
+    
     if(mode == NORMAL) {
         game_mode_str = "Normal";
     } else {
         game_mode_str = "Practice";
     }
-
+    
     cur_i = cur_j = 0;
     vector<vector<Cell>> board;
-
-    if(savedBoard == NULL) {    //generate a new board if there are no saved board
-        resetStacks();
+    
+    if(saved_board == NULL) {    //generate a new board if there are no saved board
+        reset_stacks();
         board = generate_board(board_size, debug_mode);
     } else {
         //DO NOT reset stacks if saved board is used
-        board = *savedBoard;
+        board = *saved_board;
     }
-
-    bool showHint = false;
-
+    
+    bool show_hint = false;
+    
     while(1) {
         cls();
         cout << "GAME MODE: " << game_mode_str << endl << endl;
         print_board(board);
         cout << endl;
-
+        
         //check if user win, if user asked for hint, display also the result from each checking
-        if(checkRows(board, showHint) && checkColumns(board, showHint) && checkWhite(board, showHint) && checkAdjBlackCell(board, showHint)) {
+        if(check_rows(board, show_hint) && check_columns(board, show_hint) && check_white(board, show_hint) && check_adj_black_cell(board, show_hint)) {
             cout << "YOU WIN!!!" << endl;
             cout << "Press any key to exit" << endl;
             readkey();
             return ;
-        } else if(!showHint && mode == PRACTICE) {
+        } else if(!show_hint && mode == PRACTICE) {
             //for practice mode, check if all white cell connected, if yes, check also if there are any adjacent black cell
-            checkWhite(board, true)&& checkAdjBlackCell(board, true);
+            check_white(board, true)&& check_adj_black_cell(board, true);
         }
-
-        showHint = false;
+        
+        show_hint = false;
         //display the key list
         cout << endl;
         cout << "\033[47;91mH A \033[30mLeft  \033[91mJ S \033[30mDown  \033[91mK W \033[30mUp  \033[91mL D \033[30mRight\033[91m                                           " << endl;
         cout << "(SPACE) \033[30mChange color  \033[91mE \033[30mExport  \033[91mU \033[30mUndo  \033[91mR \033[30mRedo  \033[91m? \033[30mHint  \033[91mX Q \033[30mExit                \033[0m" << endl;
-
+        
         //print undo/redo stack if debug mode enabled
         if(debug_mode) {
-            printStack();
+            print_stacks();
         }
-
+        
         //read user input
         int c = readkey();
-
+        
         if(c == 'x' || c == 'q') {      //Exit
             cout << endl << "Please press [Y] to confirm exit to main menu, or other keys to cancel" << endl;
-
+            
             if(readkey() == 'y') {
                 return;
             }
         } else if(c == '?') {           //Hint
-            showHint = true;
+            show_hint = true;
         } else if(c == 'w' || c == 'k') { //UP
             if(cur_i > 0) {
                 cur_i--;
@@ -422,7 +423,7 @@ void start_game(GAME_MODE mode, vector<vector<Cell>>* savedBoard) {
         } else if(c == 'r') {       //Redo
             redo(board);
         } else if(c == 'e') {       //Export
-            exportGame(board, mode);
+            export_game(board, mode);
         } else if(c == ' ') {       //Change color
             //change the color of current cell
             Cell e = board[cur_i][cur_j];   //save a copy of the current cell
@@ -434,7 +435,7 @@ void start_game(GAME_MODE mode, vector<vector<Cell>>* savedBoard) {
             a->from = e.status;
             a->to = board[cur_i][cur_j].status;
             //add the action to the undo stack. (note: this function will also clear the redo stack)
-            addAction(a);
+            add_action(a);
         }
     }
 }
@@ -444,38 +445,38 @@ void settings_screen() {
     cout << "Current board size: " << board_size << endl;
     cout << "New board size : ";
     cin >> board_size;
-
+    
     while(board_size < 5 || board_size > 9) {
         cout << "The board size must be between 5 and 9." << endl;
         cout << "Current board size: " << board_size << endl;
         cout << "New board size : ";
         cin >> board_size;
     }
-
+    
     cout << endl;
     cout << "Current game save path: " << save_path << endl;
     cout << "New game save path: ";
     cin >> save_path;
-
+    
     while(true) {
         ofstream test_file(save_path, fstream::in | fstream::out | fstream::app);
-
+        
         if(test_file.is_open()) {
             test_file.close();
             break;
         }
-
+        
         cout << "Invalid path" << endl;
         cout << "Current game save path: " << save_path << endl;
         cout << "New game save path: ";
         cin >> save_path;
     }
-
+    
     cout << "Press any key to continue" << endl;
     readkey();
 }
 
-//load saved game from file. (refer to exportGame() for the file format)
+//load saved game from file. (refer to export_game() for the file format)
 void load_screen() {
     cls();
     string infile_name = save_path;
@@ -491,58 +492,58 @@ void load_screen() {
     int w = board_size;
     int status_int = 0;
     vector<vector<Cell>> board(w);
-
+    
     //set the board
     for(int i = 0; i < w; i++) {
         board[i] = vector<Cell>(w);
-
+        
         for(int j = 0; j < w; j++) {
             //cout << "\rLoading board [" << i << "," << j << "]";
             infile >> board[i][j].n >> status_int;
             board[i][j].status = cell_status(status_int);
         }
     }
-
+    
     cout << endl;
-    resetStacks();  //reset the undo/redo stack
+    reset_stacks();  //reset the undo/redo stack
     int n;
     infile >> n;    //get number of action in undo stack
     infile.ignore();    //ignore the \n
     //cout << "Undo Stack Size: " << n << endl;
     string s;
     stack<Action*> tmp1;
-
+    
     //add undo action into a temperary stack
     for(int i = 0; i < n; i++) {
         getline(infile, s);
         //cout << "Loading action "<<s<<endl;
-        tmp1.push(stringToAction(s));
+        tmp1.push(string_to_action(s));
     }
-
+    
     //as undo action added into tmp1 is upside-down, pop and push into the actual undo stack
     while(tmp1.size() > 0) {
-        getUndoStack()->push(tmp1.top());
+        get_undo_stack()->push(tmp1.top());
         tmp1.pop();
     }
-
+    
     infile >> n;    //get number of action in redo stack
     infile.ignore();    //ignore the \n
     //cout << "Redo Stack Size: " << n << endl;
     stack<Action*> tmp2;
-
+    
     //add redo action into a temperary stack
     for(int i = 0; i < n; i++) {
         getline(infile, s);
         //cout << "Loading action "<<s<<endl;
-        tmp2.push(stringToAction(s));
+        tmp2.push(string_to_action(s));
     }
-
+    
     //as redo action added into tmp1 is upside-down, pop and push into the actual undo stack
     while(tmp2.size() > 0) {
-        getRedoStack()->push(tmp2.top());
+        get_redo_stack()->push(tmp2.top());
         tmp2.pop();
     }
-
+    
     infile.close();
     cout << "Loaded the file " << infile_name << ". Press any key to continue." << endl;
     cin.ignore();
@@ -550,36 +551,36 @@ void load_screen() {
     start_game(mode, &board);
 }
 
-void exportGame(vector<vector<Cell>> board, GAME_MODE mode) {
+void export_game(vector<vector<Cell>> board, GAME_MODE mode) {
     string outfile_name = save_path;
     ofstream outfile;
     outfile.open(outfile_name, ios::out);
     outfile << board_size << endl;
     outfile << mode << endl;
-
+    
     for(int i = 0; i < board.size(); i++) {
         for(int j = 0; j < board[0].size(); j++) {
             outfile << board[i][j].n << " " << board[i][j].status << endl;
         }
     }
-
+    
     outfile << endl;
-    stack<Action*> tmp1(*(getUndoStack()));
+    stack<Action*> tmp1(*(get_undo_stack()));
     outfile << tmp1.size() << endl;
-
+    
     while(tmp1.size() > 0) {
-        outfile << actionToStr(tmp1.top());
+        outfile << action_to_string(tmp1.top());
         tmp1.pop();
     }
-
-    stack<Action*> tmp2(*(getRedoStack()));
+    
+    stack<Action*> tmp2(*(get_redo_stack()));
     outfile << tmp2.size() << endl;
-
+    
     while(tmp2.size() > 0) {
-        outfile << actionToStr(tmp2.top());
+        outfile << action_to_string(tmp2.top());
         tmp2.pop();
     }
-
+    
     outfile.close();
     cout << "Saved to " << outfile_name << endl;
     cout << "Press any key to continue" << endl;
